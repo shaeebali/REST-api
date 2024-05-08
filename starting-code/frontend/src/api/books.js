bbb920330f048413dd031f47cf14f95483f0f1a4
shaeeb <shaeebali@gmail.com>
2024-05-08 11:45:55 +0100

    import API endpoint from index.js

--- starting-code/frontend/src/api/books.js
+++ starting-code/frontend/src/api/books.js
@@ -1,5 +1,5 @@
 // TODO: Import API_ENDPOINT
-
+import {API_ENDPOINT} from './index.js';
 // TODO: Create the addNewBook function that takes in newTitle, newStart, and newEnd as arguments. Inside the function, create a POST request for the new book. Store the response as a JSON in a variable called newBook and return it at the end of the function.
 
 // TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
